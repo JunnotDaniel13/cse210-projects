37cc452e863bb17874e856b4db43a2ad413cfe50
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-03-09 12:49:05 +0300

    Finished C# Programming Exercise 4: Lists and Generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        int sum = 0;
+        double average = 0;
+        int max = 0;
+        int min = 0;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine()); 
+            if (number != 0) 
+            {
+                numbers.Add(number);
+            }
+
+            sum += number; 
+            average = ((float)sum) / numbers.Count;
+
+            if (number > max) 
+            {
+                max = number;
+            } 
+
+            if (number < min) 
+            {
+                min = number;
+            } 
+
+        } while (number != 0);     
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest positive number is: {min}");
+        
+
+        numbers.Sort();
+        Console.WriteLine("Sorted numbers:");
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
