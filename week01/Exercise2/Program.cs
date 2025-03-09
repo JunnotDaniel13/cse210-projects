2b2899bf589bedf6a39c6b504f0c5b545c100c7f
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-03-09 09:12:49 +0300

    Finished C# Programming Exercise 2: If Statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        int gradeLastDigit = grade % 10;
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        } else if (grade >= 80)
+        {
+            letter = "B";
+        } else if (grade >= 70)
+        {
+            letter = "C";
+        } else if (grade >= 60)
+        {
+            letter = "D";
+        } else 
+        {
+            letter = "F";
+        }
+
+        if (gradeLastDigit >= 7) 
+        {
+            if (letter != "A" && letter != "F") 
+            {
+                letter = letter + "+";
+            }
+        } else if (gradeLastDigit < 3) 
+        {
+            if (letter != "F") 
+            {
+                letter = letter + "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is {grade}, it is a {letter}");
+
+        if (grade >= 70) {
+            Console.WriteLine("Congradulation!! You passed");
+        } else
+        {
+            Console.WriteLine("Give your best next time!");
+        }
     }
 }
