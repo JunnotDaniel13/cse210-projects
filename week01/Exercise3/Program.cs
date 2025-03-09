f30bc19ea2a781d058838ee0a92384fbd1f109c6
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-03-09 09:55:00 +0300

    Finished C# Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string playAgain = "no";
+
+        do
+        {
+            int magicNumber = randomGenerator.Next(1, 100);
+            Console.WriteLine($"{magicNumber}");
+
+            int guess = -1;
+            int attempt = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempt++;
+
+                if (guess < magicNumber) 
+                {
+                    Console.WriteLine("Higher");
+                } else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                } else {
+                    Console.WriteLine($"You guessed it! It took you {attempt} attempts");
+                }
+            } while (guess != magicNumber);
+            
+            
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain == "yes");
+
     }
 }
