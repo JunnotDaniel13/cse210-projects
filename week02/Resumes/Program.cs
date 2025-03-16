c152c677c45a10969a1160f4ab3812881e7c70e3
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-03-16 21:04:42 +0300

    Finished W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "Apple";
+        job1._jobTitle = "Full stack developer";
+        job1._startYear = 2025;
+        job1._endYear = 2030;
+
+        Job job2 = new Job();
+
+        job2._company = "Unknown";
+        job2._jobTitle = "Business Owner";
+        job2._startYear = 2030;
+        job2._endYear = 2200;
+
+
+        Resume myResume = new Resume();
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume._name = "Jerenason Junnot Daniel";
+
+        myResume.Display();
     }
 }
