5e245e1f044ef21283cb5357dc7e89caf7c3e86b
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-03-25 21:09:22 +0300

    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction(); 
+        Fraction f2 = new Fraction(5); 
+        Fraction f3 = new Fraction(3, 4); 
+
+        // Display fractions
+        Console.WriteLine(f1.GetFractionString()); 
+        Console.WriteLine(f1.GetDecimalValue());   
+
+        Console.WriteLine(f2.GetFractionString()); 
+        Console.WriteLine(f2.GetDecimalValue());   
+
+        Console.WriteLine(f3.GetFractionString()); 
+        Console.WriteLine(f3.GetDecimalValue());   
+
+        f3.SetNumerator(1);
+        f3.SetDenominator(3);
+        Console.WriteLine(f3.GetFractionString()); 
+        Console.WriteLine(f3.GetDecimalValue());   
     }
 }
