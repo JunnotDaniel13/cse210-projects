b50bebf5dacf0ab20702af2b88c5405227d9d68d
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-04-15 20:45:32 +0300

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }    
     }
 }
